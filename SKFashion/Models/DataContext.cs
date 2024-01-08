112295d5592d5a2534b4e374f5e03e0248ce9dde
Swapnil2106 <swapnillkore@gmail.com>
2024-01-08 08:31:23 +0530

    created a dbcontext class + connection string + added dependency injection in the program.cs class

---
+++ SKFashion/Models/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SKFashion.Models
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+                
+        }
+
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Address> Address { get; set; }
+        public DbSet<Cart> Cart { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+    }
+}
--- SKFashion/Program.cs
+++ SKFashion/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using SKFashion.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//added dependency injection
+builder.Services.AddDbContext<DataContext>(s => s.UseSqlServer(builder.Configuration.GetConnectionString("SKConnection")));
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
