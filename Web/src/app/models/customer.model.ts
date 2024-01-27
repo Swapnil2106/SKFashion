227d753d9f23c2e60194e6c8af32bdf257dca2ee
Swapnil2106 <swapnillkore@gmail.com>
2024-01-27 16:07:28 +0530

    Added models to the angular side

---
+++ Web/src/app/models/customer.model.ts
@@ -0,0 +1,12 @@
+export class Customer {
+
+  customerId: number | undefined;
+  firstName: string | undefined;
+  lastName: string | undefined;
+  email: string | undefined;
+  phoneNumber: number | undefined;
+  gender: string | undefined;
+  dateOfBirth: any;
+  addressId: number | undefined;
+
+}
